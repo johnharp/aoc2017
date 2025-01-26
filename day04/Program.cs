4255700c651122194c283874a9a8b1daaf886815
John Harper <john@harper.link>
2025-01-26 07:18:17 -0600

    day04 part 1

---
+++ day04/Program.cs
@@ -0,0 +1,33 @@
+﻿string[] lines = File.ReadAllLines("input.txt");
+int part1 = 0;
+
+foreach(var line in lines)
+{
+    if (isValidPassphrase(line))
+    {
+        part1++;
+    }
+}
+
+Console.WriteLine($"Part 1: {part1}");
+
+
+bool isValidPassphrase(string line)
+{
+    string[] parts = line.Split(new [] { ' ', '\t'});
+    HashSet<string> words = new HashSet<string>();
+
+    foreach(var part in parts)
+    {
+        if (words.Contains(part))
+        {
+            return false;
+        }
+        else
+        {
+            words.Add(part);
+        }
+    }
+
+    return true;
+}
