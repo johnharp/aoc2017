d04c41dde851bd8b6701c349c2c8c8116596ecaa
John Harper <john@harper.link>
2025-01-25 22:37:58 -0600

    complete day01 and day02

---
+++ day01/Program.cs
@@ -0,0 +1,31 @@
+﻿using System.Diagnostics;
+
+string[] lines = File.ReadAllLines("input.txt");
+Debug.Assert(lines.Length == 1, "Only one line of input expected");
+
+string line = lines[0];
+
+long part1 = 0;
+long part2 = 0;
+
+for (int i = 0; i<line.Length; i++)
+{
+    char curr = line[i];
+    char next = line[(i+1) % line.Length];
+    char halfway = line[(i + line.Length/2) % line.Length];
+
+    int value = int.Parse(curr.ToString());
+
+    if (curr == next)
+    {
+        part1 += value;
+    }
+
+    if (curr == halfway)
+    {
+        part2 += value;
+    }
+}
+
+Console.WriteLine($"Part 1: {part1}");
+Console.WriteLine($"Part 2: {part2}");
---
+++ day02/Program.cs
@@ -0,0 +1,42 @@
+﻿string[] lines = File.ReadAllLines("input.txt");
+
+long part1 = 0;
+long part2 = 0;
+
+foreach(string line in lines)
+{
+    var values = line.Split(new [] {' ', '\t' }, StringSplitOptions.RemoveEmptyEntries)
+        .ToList()
+        .Select(s => long.Parse(s))
+        .ToList();
+
+    long max = values.Max();
+    long min = values.Min();
+
+    part1 += max - min;
+
+    bool found  = false;
+    for (int i = 0; !found && (i < values.Count - 1); i++)
+    {
+        for (int j = i + 1; !found && (j < values.Count); j++)
+        {
+            long v1 = values[i];
+            long v2 = values[j];
+
+            if (isEvenlyDivisible(v1, v2))
+            {
+                part2 += Math.Max(v1, v2) /
+                         Math.Min(v1, v2);
+                found = true;
+            }
+        }
+    }
+}
+
+Console.WriteLine($"Part 1: {part1}");
+Console.WriteLine($"Part 2: {part2}");
+
+bool isEvenlyDivisible(long a, long b)
+{
+    return (Math.Max(a, b) % Math.Min(a, b)) == 0;
+}
