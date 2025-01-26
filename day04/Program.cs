e5f60e7dd94655e130246737eff9e981bdfef64c
John Harper <john@harper.link>
2025-01-26 10:22:41 -0600

    day 04

--- day04/Program.cs
+++ day04/Program.cs
@@ -1,18 +1,19 @@
 ﻿string[] lines = File.ReadAllLines("input.txt");
-int part1 = 0;
 
-foreach(var line in lines)
-{
-    if (isValidPassphrase(line))
-    {
-        part1++;
-    }
-}
+
+// C# equivalent to Javascript .reduce() is .Aggregate()
+int part1 = lines
+    .Aggregate(0, (acc, x) => acc + (isPart1ValidPassphrase(x) ? 1 : 0));
+
+int part2 = lines
+    .Aggregate(0, (acc, x) => acc + (isPart2ValidPassphrase(x) ? 1 : 0));
+
 
 Console.WriteLine($"Part 1: {part1}");
+Console.WriteLine($"Part 2: {part2}");
 
 
-bool isValidPassphrase(string line)
+bool isPart1ValidPassphrase(string line)
 {
     string[] parts = line.Split(new [] { ' ', '\t'});
     HashSet<string> words = new HashSet<string>();
@@ -27,6 +28,51 @@ bool isValidPassphrase(string line)
         {
             words.Add(part);
         }
+    }
+
+    return true;
+}
+
+bool isPart2ValidPassphrase(string line)
+{
+    var counts = line.Split(new [] { ' ', '\t'})
+        .Select(doCharCount)
+        .ToList();
+
+    for (int i = 0; i < counts.Count - 1; i++)
+    {
+        for (int j = i+1; j < counts.Count; j++)
+        {
+            if (countsAreSame(counts[i], counts[j])) return false;
+        }
+    }
+
+    return true;
+}
+
+List<(char, int)> doCharCount(string word)
+{
+    return word
+        .GroupBy(l => l)
+        .Select(g => (g.Key, g.Count()))
+        .OrderBy(i => i.Item1)
+        .ToList();
+}
+
+bool countsAreSame(List<(char, int)> c1, List<(char, int)> c2)
+{
+    // For the counts to be the same, they must have the same number
+    // of unique characters
+    if (c1.Count() != c2.Count) return false;
+
+    // Also, the list of characters must be the same
+    string c1chars = String.Join("", c1.Select(i => i.Item1));
+    string c2chars = String.Join("", c2.Select(i => i.Item1));
+    if (c1chars != c2chars) return false;
+
+    for (int i = 0; i < c1.Count; i++)
+    {
+        if (c1[i].Item2 != c2[i].Item2) return false;
     }
 
     return true;
