ff82c4cb316da0b4168912d0fb46fb331ac8010e
John Harper <john@harper.link>
2025-01-26 17:38:20 -0600

    day 07 -- still needs refactoring, however.

---
+++ day07/Node.cs
@@ -0,0 +1,148 @@
+using System.Diagnostics.Contracts;
+
+public class Node
+{
+    private static Dictionary<string, Node> _AllNodes = new Dictionary<string, Node>();
+    public static string? RootNodeId = null;
+
+    public static void AddNode(Node n)
+    {
+        _AllNodes[n.Id] = n;
+    }
+
+    public static List<Node> AllNodes
+    {
+        get
+        {
+            List<Node> all = new List<Node>();
+
+            foreach(var id in _AllNodes.Keys)
+            {
+                Node n = _AllNodes[id];
+                all.Add(n);
+            }
+
+            return all;
+        }
+    }
+
+
+    string? _ParentId = null;
+    string _Id;
+    int _Weight;
+    int _TotalWeight = -1;
+
+    List<string> _ChildIds;
+
+    public string Id
+    {
+        get { return _Id; }
+    }
+
+    public List<string> ChildIds
+    {
+        get { return _ChildIds; }
+    }
+
+    public List<Node> Children
+    {
+        get
+        {
+            List<Node> children = new List<Node>();
+            foreach(var childid in _ChildIds)
+            {
+                Node n = _AllNodes[childid];
+                children.Add(n);
+            }
+            return children;
+        }
+    }
+
+    public string? ParentId
+    {
+        get { return this._ParentId; }
+        set { _ParentId = value; }
+    }
+
+    public int Weight
+    {
+        get { return _Weight; }
+    }
+
+    public int TotalWeight
+    {
+        get
+        {
+            if (_TotalWeight == -1)
+            {
+                int childrenWeight = 0;
+                foreach(string childid in _ChildIds)
+                {
+                    Node child = _AllNodes[childid];
+                    childrenWeight += child.TotalWeight;
+                }
+                _TotalWeight = childrenWeight + _Weight;
+            }
+            return _TotalWeight;
+        }
+    }
+
+    public List<int> ChildWeights
+    {
+        get
+        {
+            List<int> childWeights = new List<int>();
+            foreach(Node child in Children)
+            {
+                childWeights.Add(child.Weight);
+            }
+
+            return childWeights;
+        }
+    }
+
+    public List<int> ChildTotalWeights
+    {
+        get
+        {
+            List<int> totalWeights = new List<int>();
+
+            foreach(string childid in ChildIds)
+            {
+                Node child = _AllNodes[childid];
+                totalWeights.Add(child.TotalWeight);
+            }
+
+            return totalWeights;
+        }
+    }
+
+    public Node(string id, int weight, List<string> childIds)
+    {
+        _Id = id;
+        _Weight = weight;
+        _ChildIds = new List<string>(childIds);
+    }
+
+    public static void LinkParents()
+    {
+        foreach(string nodeid in _AllNodes.Keys)
+        {
+            Node node = _AllNodes[nodeid];
+            foreach(var childId in node.ChildIds)
+            {
+                Node child = _AllNodes[childId];
+                child.ParentId = nodeid;
+            }
+        }
+
+        foreach(string nodeid in _AllNodes.Keys)
+        {
+            Node node = _AllNodes[nodeid];
+            if (node.ParentId == null)
+            {
+                RootNodeId = nodeid;
+            }
+        }
+    }
+}
---
+++ day07/Program.cs
@@ -0,0 +1,50 @@
+﻿namespace day07;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string[] lines = File.ReadAllLines("input.txt");
+
+        foreach(var line in lines)
+        {
+            HandleLine(line);
+        }
+        Node.LinkParents();
+
+        Console.WriteLine($"Part 1: {Node.RootNodeId}");
+
+        var all = Node.AllNodes;
+        foreach(var node in all)
+        {
+            var ChildTotalWeights = node.ChildTotalWeights;
+            if (ChildTotalWeights.Distinct().Count() > 1)
+            {
+                Console.WriteLine($"Node {node.Id} with weight {node.Weight} has children towers with differing weights:");
+                foreach(var c in node.Children)
+                {
+                    Console.WriteLine($"\tchild {c.Id} has Weight {c.Weight} and TotalWeight {c.TotalWeight}");
+                }
+            }
+        }
+    }
+
+    static void HandleLine(string line)
+    {
+        string[] parts = line.Split(" -> ");
+        string[] leftParts = parts[0].Split(" ");
+
+        string id = leftParts[0];
+        int weight = int.Parse(leftParts[1].Replace("(", "").Replace(")", ""));
+        var childIds = new List<String>();
+
+        if (parts.Length == 2)
+        {
+           childIds = parts[1].Split(", ").ToList();
+        }
+
+        Node n = new Node(id, weight, childIds);
+        Node.AddNode(n);
+    }
+
+}
